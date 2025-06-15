2a61cb6e327b9f406f8179a3e963a2bc544b8f9c
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-06-15 13:46:38 +0800

    Tool

--- Tool/Z.Tool.Tool.IconData/-.cs
+++ Tool/Z.Tool.Tool.IconData/-.cs
@@ -1,3 +1,6 @@
+global using String = Avalon.Infra.String;
+global using Array = Avalon.List.Array;
+global using Text = Avalon.Text.Text;
 global using EntryEntry = Avalon.Entry.Entry;
 global using ToolBase = Z.Infra.Infra.Base;
 global using STAThreadAttribute = System.STAThreadAttribute;
--- Tool/Z.Tool.Tool.IconData/Gen.cs
+++ Tool/Z.Tool.Tool.IconData/Gen.cs
@@ -4,6 +4,48 @@ class Gen : ToolBase
 {
     public virtual long Execute()
     {
+        String iconListPath;
+        iconListPath = this.S("ToolData/Tool/ItemListIcon.txt");
+
+        String iconListText;
+        iconListText = this.StorageTextRead(iconListPath);
+
+        Array lineList;
+        lineList = this.StringLine(this.TA(iconListText));
+
+        Text kka;
+        kka = this.TextCreate(this.S(" "));
+
+        long count;
+        count = lineList.Count;
+        long i;
+        i = 0;
+        while (i < count)
+        {
+            String line;
+            line = lineList.GetAt(i) as String;
+
+            Text k;
+            k = this.TextCreate(line);
+
+            Array array;
+            array = this.TextLimit(k, kka);
+
+            Text ka;
+            Text kb;
+            ka = array.GetAt(0) as Text;
+            kb = array.GetAt(1) as Text;
+
+            String name;
+            name = this.StringCreate(ka);
+
+            String filePath;
+            filePath = this.AddClear().AddS("../../Icon/").Add(name).AddS(".png").AddResult();
+
+            this.StorageInfra.DataRead(filePath);
+
+            i = i + 1;
+        }
         return 0;
     }
 }
