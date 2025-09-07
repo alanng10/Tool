3100b0fb3eefa6f6b214aa5771275d95780a4307
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-09-07 16:40:24 +0800

    Script

--- Script/Module/Make.cmd
+++ Script/Module/Make.cmd
@@ -3,6 +3,7 @@
 call Script\Module\MakeModule Icon
 call Script\Module\MakeModule View
 call Script\Module\MakeModule Package
+call Script\Module\MakeModule Struct
 call Script\Module\MakeModule Problem
 call Script\Module\MakeModule "Find"
 call Script\Module\MakeModule Docue
